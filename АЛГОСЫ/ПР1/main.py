bfa3841793414e140a59893da3eadc926e99de89
OneTwoZzzPlus <76748058+OneTwoZzzPlus@users.noreply.github.com>
2024-09-14 00:50:11 +0300

    algos-hw1: 2 and 3 problems

--- АЛГОСЫ/ПР1/main.py
+++ АЛГОСЫ/ПР1/main.py
@@ -1,28 +1,29 @@
 from data import *
 
-print('Программа создана, чтобы угадывать однокурсников "инфокома", которых вы загадали!')
-print('Вводите ответ на вопрос одним словом или символом: +/-, да/нет, true/false, 1/0 etc.')
-step = -1
+repeat = True
+print('''Программа создана, чтобы угадывать однокурсников и адаптеров K3120 и K3121!'
+Вводите ответ на вопрос одним словом или символом: +/-, да/нет, true/false, 1/0 etc.
+Для выхода введите exit или нажмите ctrl+C.''')
+
+step, people_keys = -1, []
 while True:
     # Запуск
-    if step < 0:
-        print('\nНачинаем игру!')
+    if step < 0 or step >= len(questions):
+        print(f'\nНачинаем игру!')
         step = 0
         key = ''
         people_keys = sorted(people.keys())
-    # Дополнительный вопрос
-    elif step >= len(questions):
-        print('УНИКАЛЬНЫЙ ВОПРОС')
 
     # Вопрос
     print(questions[step])
+
     # Ввод ответа
     inp = None
     while inp is None:
         try:
             # Получение строки от пользователя в нижнем регистре, опуская пробелы и точки
             raw = input('>>> ').lower().replace(' ', '').replace('.', '')
-            # Поиск значения
+            # Поиск значения ввода
             if raw == 'exit':
                 exit(0)
             elif raw in true_answers:
@@ -33,23 +34,25 @@ while True:
                 raise ValueError
         except (EOFError, ValueError):
             # Ошибка при вводе
-            print("Некорректный ввод!")
+            print('Некорректный ввод!')
         except KeyboardInterrupt:
             exit(0)
-    # Обработка шага
-    inp_v = '1' if inp else '0'
+
     # Убираем из списка всех не подходящих по характеристике
+    inp_v = '1' if inp else '0'
     people_keys = [x for x in people_keys if x[step] == inp_v]
 
     if len(people_keys) == 1:
         # Отгадали, если остался только 1
         print(f'Ваш загаданный однокурсник {people[people_keys[0]]}!')
-        step = -1
-        # exit(0)
+        if repeat:
+            step = -1  # Перезапуск
+        else:
+            input('Нажмите enter для выхода...')
+            exit(0)  # Выход
     else:
         print(f'Выбираем из {len(people_keys)} человек.')
-        print(f'{bcolors.WARNING}{people_keys}{bcolors.ENDC}')
+        # print(people_keys)
         # Если на следующий вопрос у всех кандидатов одинаковая характеристика - пропускаем вопрос
         while all(x[step] == people_keys[0][step] for x in people_keys):
             step += 1
-
